84cc664fa7f720e54c316b8e62f320e16670be63
Bruno Zell <bruno.zzell@gmail.com>
2020-03-23 03:08:34 +0100

    add client project

---
+++ NSwagTest.Client/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace NSwagTest.Client
+{
+    public class Class1
+    {
+        void Main()
+        {
+            var client = new Rabot.Swagger.NSwagTest_Server_NSwagTestApiClient("http://localhost:5000", new System.Net.Http.HttpClient());
+        }
+    }
+}
