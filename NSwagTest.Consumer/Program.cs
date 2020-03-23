3609e3f395408f28c3a1110dff1745b0dc37bc23
Bruno Zell <bruno.zzell@gmail.com>
2020-03-23 03:12:07 +0100

    add separate consumer project

---
+++ NSwagTest.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSwagTest.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client.NSwagTestApiClient("http://localhost:5000", new System.Net.Http.HttpClient());
+        }
+    }
+}
